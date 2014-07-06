31da57cd23ca4b1017b33b416811042488a4e949
masascii <sdmshr1248@gmail.com>
2014-07-06 14:46:23 +0900

    spec5: レシピのID振り分け

--- omuraisu.java
+++ omuraisu.java
@@ -2,14 +2,15 @@ import java.io.*;
 public class omuraisu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String str = null;
+		int id = 1;//idは1から始める
 		try {
 			BufferedReader br_filename = new BufferedReader(new InputStreamReader(System.in));
 			BufferedReader br_file = new BufferedReader(new FileReader(new File(br_filename.readLine())));
 			
 			while((str = br_file.readLine()) != null) {
-				System.out.println(str);
+				System.out.println(id + ":" + str);
+				id++;
 			}
 			
 		}catch(IOException e) {
