8d1003ffbc4bc07660c41a14198ad53cbef95ef9
masascii <sdmshr1248@gmail.com>
2014-07-06 14:42:25 +0900

    spec4: データファイルに３つのデータを保存

--- omuraisu.java
+++ omuraisu.java
@@ -3,11 +3,15 @@ public class omuraisu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String str = null;
 		try {
 			BufferedReader br_filename = new BufferedReader(new InputStreamReader(System.in));
 			BufferedReader br_file = new BufferedReader(new FileReader(new File(br_filename.readLine())));
-		
-			System.out.println(br_file.readLine());
+			
+			while((str = br_file.readLine()) != null) {
+				System.out.println(str);
+			}
+			
 		}catch(IOException e) {
 			System.err.println(e);
 		}
