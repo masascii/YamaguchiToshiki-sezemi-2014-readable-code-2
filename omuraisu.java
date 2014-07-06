c51be082ca75202c9f157615c7a38a7bcc63c435
masascii <sdmshr1248@gmail.com>
2014-07-06 14:33:24 +0900

    spec3: レシピデータを別ファイルに分離

--- omuraisu.java
+++ omuraisu.java
@@ -1,11 +1,16 @@
-
+import java.io.*;
 public class omuraisu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			BufferedReader br_filename = new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader br_file = new BufferedReader(new FileReader(new File(br_filename.readLine())));
 		
-		
-		System.out.println("オムライス");
+			System.out.println(br_file.readLine());
+		}catch(IOException e) {
+			System.err.println(e);
+		}
 		
 	}
 
