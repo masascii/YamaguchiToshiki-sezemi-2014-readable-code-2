62b7fdf80461c3ddd817ba7559f619c162161ad4
masascii <sdmshr1248@gmail.com>
2014-07-06 15:14:18 +0900

    spec6: 指定したIDの出力

--- omuraisu.java
+++ omuraisu.java
@@ -1,22 +1,30 @@
 import java.io.*;
 public class omuraisu {
-
+	//TO DO while文のモジュール化
 	public static void main(String[] args) {
 		String str = null;
+		String[] input;
 		int id = 1;//idは1から始める
 		try {
 			BufferedReader br_filename = new BufferedReader(new InputStreamReader(System.in));
-			BufferedReader br_file = new BufferedReader(new FileReader(new File(br_filename.readLine())));
-			
-			while((str = br_file.readLine()) != null) {
-				System.out.println(id + ":" + str);
-				id++;
+			input = br_filename.readLine().split(" ");
+			BufferedReader br_file = new BufferedReader(new FileReader(new File(input[0])));
+			if(input.length > 1) {
+				while((str = br_file.readLine()) != null) {
+					if(id == Integer.parseInt(input[1])) {
+						System.out.println(id + ":" + str);
+						break;
+					}
+					id++;
+				}
+			}else {
+				while((str = br_file.readLine()) != null) {
+					System.out.println(id + ":" + str);
+					id++;
+				}
 			}
-			
 		}catch(IOException e) {
 			System.err.println(e);
 		}
-		
 	}
-
 }
